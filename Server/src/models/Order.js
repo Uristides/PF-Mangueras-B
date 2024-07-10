ad04d228eff15c33dbdd5fc3a67d2677154b48c2
Uristides <120599091+Uristides@users.noreply.github.com>
2024-07-10 14:12:12 -0600

    Archivo parceo(descomentar en server la primera vez)

--- Server/src/models/Order.js
+++ Server/src/models/Order.js
@@ -1,22 +1,26 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("order", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true
+  sequelize.define(
+    "order",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    amount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  }, {timestamps: false});
+    { timestamps: false }
+  );
 };
