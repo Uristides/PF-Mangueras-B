9cfcaeb1efec1bb112a58df429e94c868c54f498
Uristides <120599091+Uristides@users.noreply.github.com>
2024-07-11 13:56:08 -0600

    agregado cambios de facu

--- Server/src/routes/userRouters.js
+++ Server/src/routes/userRouters.js
@@ -1,11 +1,16 @@
 const { Router } = require("express");
+const registerUserHandler = require("../handlers/userHandlers/postRegisterHandler");
+const getUsersHandler = require("../handlers/userHandlers/getUsersHandlers");
+const loginUserHandler = require("../handlers/userHandlers/postLoginHandler");
+const protectedHandler = require("../handlers/userHandlers/getProtectHandler");
+const logoutHandler = require("../handlers/userHandlers/getLogoutHandler");
 
-const userRoutes = Router();
+const UserRoutes = Router();
 
-//userRoutes.get("/", getUsersHandler);
+UserRoutes.post("/register", registerUserHandler);
+UserRoutes.post("/login", loginUserHandler);
+UserRoutes.get("/", getUsersHandler);
+UserRoutes.get("/protected", protectedHandler);
+UserRoutes.get("/logout", logoutHandler);
 
-// productRoutes.get("/:id", )
-
-//userRoutes.post("/", postCreateUser);
-
-module.exports = userRoutes;
+module.exports = UserRoutes;
