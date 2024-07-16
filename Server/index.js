e02f0b453e63bcb9d45cc168c6b128bd471920ac
Gcasa28 <jacasanova.vt@gmail.com>
2024-07-16 17:08:31 -0400

    config de deploy

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,9 @@
 const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 conn
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
