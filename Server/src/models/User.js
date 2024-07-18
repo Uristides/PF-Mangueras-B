1e21df0cafd7ca9565a88cda75ba9c887f4f7dea
Uristides <120599091+Uristides@users.noreply.github.com>
2024-07-18 00:26:22 -0600

    cambio en modelo user

--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -26,6 +26,7 @@ module.exports = (sequelize) => {
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
       },
       tercero: {
         type: DataTypes.BOOLEAN,
