9d752f250ac58f3f3af84574583ccddb58e261fd
Uristides <120599091+Uristides@users.noreply.github.com>
2024-07-17 20:34:21 -0600

    prueba 2

--- Server/src/db.js
+++ Server/src/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-//comentar 19082 si se ejeca en local
+//comentar :19082 si se ejeca en local
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}19082/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:19082/${DB_NAME}`,
   {
     logging: false,
     native: false,
