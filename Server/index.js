909b1378b868de603f52a19b70c91b150f8de6e1
Uristides <120599091+Uristides@users.noreply.github.com>
2025-05-29 06:15:52 -0600

    Deteccion de error con base de datos

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,13 @@
-const server = require("./src/server.js");
-const { conn } = require("./src/db.js");
-const PORT = process.env.PORT || 3001;
+console.log("Intentando conectar a la base de datos...");
 
 conn
   .sync({ force: false })
   .then(() => {
+    console.log("Base de datos conectada");
     server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+      console.log(`Servidor escuchando en el puerto ${PORT}`);
     });
   })
-  .catch((error) => console.error(error));
+  .catch((error) => {
+    console.error("Error al conectar a la base de datos:", error);
+  });
